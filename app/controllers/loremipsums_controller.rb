3fe849e089c29fb835c9c6b45f2d4aa3c5b60551
amzotti <azotti1337@gmail.com>
2014-10-25 15:59:25 -0700

    User can view auto generated text

--- app/controllers/loremipsums_controller.rb
+++ app/controllers/loremipsums_controller.rb
@@ -1,5 +1,5 @@
 class LoremipsumsController < ApplicationController
   def index
-
+    @text = Generator.generate_text(5)
   end
 end
---
+++ app/models/generator.rb
@@ -0,0 +1,12 @@
+class Generator 
+
+  SIZE_OF_PARAGRAPHS = 10
+
+  def self.generate_text(number_of_paragraphs)
+    @text = ""
+    number_of_paragraphs.times do
+      @text += "<p>" + Forgery(:lorem_ipsum).paragraphs(SIZE_OF_PARAGRAPHS) + "</p>"
+    end
+    @text
+  end
+end
